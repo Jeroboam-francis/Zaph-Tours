1e553ec351b174e278d841c6d60fb35772975e8b
jeroboam-francis <francisjeroboam@gmail.com>
2025-03-09 00:08:52 +0300

    Build Hero section

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,24 @@
+import "./Home.css";
 function Home() {
   return (
     <div>
-      <h1> Home</h1>
+      <h1>
+        <Hero />
+      </h1>
     </div>
   );
 }
 
 export default Home;
+
+function Hero() {
+  return (
+    <div className="hero">
+      <h1>Explore the Beauty of Kenya</h1>
+      <p>
+        Experience your adventures with the best safari guides in East Africa
+      </p>
+      <button>Book Now</button>
+    </div>
+  );
+}
